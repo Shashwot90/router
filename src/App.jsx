3555f7988f9e21a6c22db7b2daad45182b3073c5
Shashwot Pradhan <shashwotpradhan@gmail.com>
2022-09-05 17:43:21 +0545

    'menucomponent'

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import { Route, Routes } from 'react-router-dom';
 import About from './About';
 import Contact from './Contact';
 import Error from './Error';
+import Menu from './Menu';
 
 function App() {
+  const Name = () => {
+    return (
+      <>
+        <h1>This is Name page</h1>
+      </>
+    )
+  }
   return (
    <>
+   <Menu />
+
    <Routes>
     <Route exact path='/' element={<About />} />
-    <Route path='/contact' element={<Contact />} />
+    <Route exact path='/contact' element={<Contact />} />
+    <Route path='/contact/Name' element={<Name />} />
     <Route path='*' element={<Error />}/>
    </Routes>
    </>
